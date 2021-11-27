3a7e1904b82c921c164d04bc524c18c411df0ff1
Adilene Bejines <adi3511@hotmail.com>
2021-11-26 22:10:01 -0700

    linked javascript to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,14 @@ const submitBtn = document.getElementById('submit');
 const highscoreBtn = document.getElementById('highscores');
 const userScore = document.getElementById('user-score');
 
-setInterval (myTimer, 1000);
+//setInterval (myTimer, 1000);
 
 
 function buildQuiz() {}
-
 function showResults () {}
 
 //display quiz 
-buildQuiz();
+buildQuiz();       
 
 //on submit, show results
 submitBtn.addEventListener('click', showResults);
