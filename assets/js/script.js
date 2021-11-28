7960ac0708febd03847b64cdf156380d8ec215c2
Adilene Bejines <adi3511@hotmail.com>
2021-11-28 00:29:22 -0700

    added questions to quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,57 @@
-const startBtn = document.getElementById('start');
-const nextBtn = document.getElementById('next');
-const submitBtn = document.getElementById('submit');
-const highscoreBtn = document.getElementById('highscores');
-const userScore = document.getElementById('user-score');
+var startBtn = document.getElementById('startBtn');
+var nextBtn = document.getElementById('nextBtn');
+var submitBtn = document.getElementById('submitBtn');
+var highscoreBtn = document.getElementById('highscoreBtn');
+var userScore = document.getElementById('userScore');
+var quizQuestions = document.getElementById("quizQuestions");
+var timer = document.getElementById("timer");
+var timecounter = document.getElementById("timecounter");
 
+var allScores = [];
+var storedScores = JSON.parse(localStorage.getItem("userData"));
+var questions = [
+    //Question 1
+    {
+        title: "What does HTML stand for?",
+        choices: ["Hypertext Markup Language", "Hyperspace Makeup Language", "Hightext Made up Language", "HighChase Mega Language"],
+        answer: "Hypertext Markup Language"
+    },
+    //Question 2
+    { 
+        title: "Arrays in JavaScript can be used to store...?",
+        choices: ["booleances", "other Arrays", "numbers and strings", "all of the above"],
+        answer: "all of the above"
+    },
+    //Question 3
+    {
+        title: "What does CSS stand for?",
+        choices: ["Colorful Style Sheets", "Complicated Styling Stuff", "Cascading Style Sheets", "Cynical Style Sheets"],
+        answer: "Cascading Style Sheets"
+    },
+    //Question 4
+    {
+        title: "WHat is the correct HTML for referring to an external style sheet?",
+        choices: ["<style src ='mystyle.css'>", "<link rel='stylesheet' type='text/css' href='mystyle.css'>", "<stylesheet>mystyle.css</stylesheet>", "none of the above"],
+        answer: "<link rel='stylesheet' type='text/css' href='mystyle.css'>"
+    },
+    //Question 5
+    {
+        title: "How do you insert a comment in a CSS file?",
+        choices: ["//this is a comment", "/* this is a comment*/", 'this is a comment', "//this is a comment//"],
+        answer: "/*this is a comment*/"
+    },
+]
 //setInterval (myTimer, 1000);
 
 
-function buildQuiz() {}
+startBtn.addEventListener("click", startQuiz);
+function startQuiz() {
+
+}
 function showResults () {}
 
 //display quiz 
-buildQuiz();       
+startQuiz();       
 
 //on submit, show results
 submitBtn.addEventListener('click', showResults);
